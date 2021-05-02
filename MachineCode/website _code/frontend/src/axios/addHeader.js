2fd96d5174f7aba98fa88b168d01e61e44cc951b
Farooq <mralemamygmail.com>
2021-05-01 23:58:15 -0700

    fix.

--- MachineCode/website _code/frontend/src/axios/addHeader.js
+++
@@ -1,8 +0,0 @@
-export default function authHeader() {
-
-  if (localStorage.getItem('jwt') !== null) {
-    return { Authorization: 'Bearer ' + localStorage.getItem('jwt') };
-  } else {
-    return null;
-  }
-}
--- MachineCode/website _code/frontend/src/common/index.js
+++
@@ -1,2 +0,0 @@
-export { default as FormatDate } from './FormatDate';
-export { default as ParseJWT } from './ParseJWT';
